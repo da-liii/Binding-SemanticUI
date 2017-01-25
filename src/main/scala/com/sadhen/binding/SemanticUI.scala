3ea111bf51fa001395939fabed32520eac65058f
Darcy Shen <sadhen@zoho.com>
2017-01-25 11:39:46 +0800

    add modal for semantic jquery

--- src/main/scala/com/sadhen/binding/SemanticUI.scala
+++ src/main/scala/com/sadhen/binding/SemanticUI.scala
@@ -12,6 +12,7 @@ object SemanticUI {
   trait SemanticJQuery extends JQuery {
     def dropdown(params: js.Any*): SemanticJQuery = js.native
     def search(params: js.Any*): SemanticJQuery = js.native
+    def modal(params: js.Any*): SemanticJQuery = js.native
   }
 
   implicit def jq2semantic(jq: JQuery): SemanticJQuery = jq.asInstanceOf[SemanticJQuery]
