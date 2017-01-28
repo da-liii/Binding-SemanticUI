ef6ed58581e35cc62217b8a6b83edb58a2b48b5f
Darcy Shen <sadhen@zoho.com>
2017-01-29 00:07:13 +0800

    use js.Dynamic as data

--- src/main/scala/com/sadhen/binding/component/datadisplay/TableBuilder.scala
+++ src/main/scala/com/sadhen/binding/component/datadisplay/TableBuilder.scala
@@ -5,6 +5,8 @@ import com.thoughtworks.binding.Binding.{Var, Vars}
 import com.thoughtworks.binding.{Binding, dom}
 import org.scalajs.dom.raw.Node
 
+import scala.scalajs.js
+
 /**
   * Created by rendong on 17/1/25.
   */
@@ -13,12 +15,12 @@ trait Column {
   val dataIndex: String
   val key: String
   @dom
-  def render(data: Map[String, Any]): Binding[Node] =
-    <td>{ data(dataIndex).toString }</td>
+  def render(data: js.Dynamic): Binding[Node] =
+    <td>{ data.selectDynamic(dataIndex).toString }</td>
 }
 
 case class TableBuilder() extends ComponentBuilder {
-  var dataSource: Var[Array[Map[String, Any]]] = Var(Array.empty)
+  var dataSource: Var[Array[js.Dynamic]] = Var(Array.empty)
   var columns: Var[Array[Column]] = Var(Array.empty)
 
   def render = this
