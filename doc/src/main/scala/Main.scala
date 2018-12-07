e6db9c811b17baaa4faf38c68ae9c82e7a4408e1
Darcy Shen <sadhen@zoho.com>
2018-12-08 04:56:12 +0800

    More support for Icon

--- doc/src/main/scala/Main.scala
+++ doc/src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import com.thoughtworks.binding.dom
 import org.scalajs.dom.document
 import com.sadhen.binding.component.tag._
+import com.sadhen.binding.component.autoVar
 import com.thoughtworks.binding.Binding.Var
 
 import scala.scalajs.js.annotation.JSExport
@@ -11,7 +12,29 @@ object Main {
   def icon = {
     <div>
       <hr></hr>
-      <Icon type={ Var("archive") }></Icon>
+      <div>
+        <Icon type="users" disabled={ true } />
+      </div>
+      <hr></hr>
+      <div>
+        <Icon type="spinner" loading={true}></Icon>
+        <Icon type="notched" loading={true}></Icon>
+        <Icon type="asterick" loading={true}></Icon>
+      </div>
+      <hr></hr>
+        <Icon type="users" color="red" />
+        <Icon type="users" color="orange" />
+        <Icon type="users" color="yellow" />
+        <Icon type="users" color="olive" />
+        <Icon type="users" color="green" />
+        <Icon type="users" color="teal" />
+        <Icon type="users" color="blue" />
+        <Icon type="users" color="violet" />
+        <Icon type="users" color="purple" />
+        <Icon type="users" color="pink" />
+        <Icon type="users" color="brown" />
+        <Icon type="users" color="grey" />
+        <Icon type="users" color="black" />
       <hr></hr>
     </div>
   }
--- semantic/src/main/scala/com/sadhen/binding/component/general/IconBuilder.scala
+++ semantic/src/main/scala/com/sadhen/binding/component/general/IconBuilder.scala
@@ -7,11 +7,33 @@ import org.scalajs.dom.raw.Node
 
 case class IconBuilder() extends ComponentBuilder {
   var `type`: Var[String] = Var("")
+  var disabled: Var[Boolean] = Var(false)
+  var loading: Var[Boolean] = Var(false)
+  var color: Var[String] = Var("")
 
   def render = this
 
+  def fDisabled(disabled: Boolean): String = {
+    if (disabled) "disabled"
+    else ""
+  }
+
+  def fLoading(loading: Boolean): String = {
+    if (loading) "loading"
+    else ""
+  }
+
   @dom
   override def build: Binding[Node] = {
-    <i class={`type`.bind.toString + " icon"}></i>
+    <i class={
+      List(
+        color.bind,
+        `type`.bind,
+        fLoading(loading.bind),
+        fDisabled(disabled.bind),
+        "icon"
+      ).mkString(" ")
+    }>
+    </i>
   }
 }
