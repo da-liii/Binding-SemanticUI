82bbfc7a5ab3a2acb84bfa86eae9d97a63fe774a
Darcy Shen <sadhen@zoho.com>
2018-12-09 09:01:51 +0800

    Polish documents on Icon

--- semantic/src/main/scala/com/sadhen/binding/component/general/IconBuilder.scala
+++ semantic/src/main/scala/com/sadhen/binding/component/general/IconBuilder.scala
@@ -5,22 +5,31 @@ import com.thoughtworks.binding.{Binding, dom}
 import com.thoughtworks.binding.Binding.Var
 import org.scalajs.dom.raw.Node
 
+/**
+  * Ref:
+  *   - [[https://semantic-ui.com/elements/icon.html]]
+  *   - [[https://ant.design/components/icon/]]
+  */
 case class IconBuilder() extends ComponentBuilder {
+  /** Type(name) of an icon, which determines the shape of an icon */
   var `type`: Var[String] = Var("")
+  /** State of an icon, enabled by default */
   var disabled: Var[Boolean] = Var(false)
+  /** State of an icon, static(not loading) by default */
   var loading: Var[Boolean] = Var(false)
+  /** Color of an icon */
   var color: Var[String] = Var("")
 
-  val constAttr = "icon"
+  private val constAttr = "icon"
 
   def render = this
 
-  def fDisabled(disabled: Boolean): String = {
+  private def fDisabled(disabled: Boolean): String = {
     if (disabled) "disabled"
     else ""
   }
 
-  def fLoading(loading: Boolean): String = {
+  private def fLoading(loading: Boolean): String = {
     if (loading) "loading"
     else ""
   }
