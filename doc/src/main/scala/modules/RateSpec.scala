fea3306f47359b453f86150a979c1569d60e8971
Darcy Shen <sadhen@zoho.com>
2019-01-01 13:51:09 +0800

    Polish on RateSpec

--- doc/src/main/scala/modules/RateSpec.scala
+++ doc/src/main/scala/modules/RateSpec.scala
@@ -12,48 +12,44 @@ object RateSpec extends MainEntry {
   @ant
   override def main: Ant = {
     <div class="main ui container">
+      <h1 class="ui header">
+        Rating
+        <div class="sub header">
+          A rating indicates user interest in content 
+        </div>
+      </h1>
       <h2 class="ui dividing header">Types</h2>
 
-      <div class="example">
-        <h4 class="ui header">Rating</h4>
-        <p>A basic rating </p>
-        <Rate count={1} value={0}></Rate>
-      </div>
+      <h4 class="ui header">Rating</h4>
+      <p>A basic rating </p>
+      <Rate count={1} value={0}></Rate>
 
-      <div class="example">
-        <h4 class="ui header">Star</h4>
-        <p>A rating can use a set of star icons</p>
+      <h4 class="ui header">Star</h4>
+      <p>A rating can use a set of star icons</p>
 
-        Rating
-        <Rate count={4} value={3} star={true}></Rate>
-      </div>
+      Rating<Rate count={4} value={3} star={true}></Rate>
 
-      <div class="example">
-        <h4 class="ui header">Heart</h4>
-        <p>A rating can use a set of heart icons</p>
-        <Rate count={3} value={1} heart={true}></Rate>
-      </div>
+      <h4 class="ui header">Heart</h4>
+      <p>A rating can use a set of heart icons</p>
+      <Rate count={3} value={1} heart={true}></Rate>
 
       <h2 class="ui dividing header">Variations</h2>
 
-      <div class="example">
-        <h4 class="ui header">Size</h4>
-        <p>A rating can vary in size</p>
-        <Rate count={4} value={3} star={true} size={"mini"}></Rate>
-        <br></br>
-        <Rate count={4} value={3} star={true} size={"tiny"}></Rate>
-        <br></br>
-        <Rate count={4} value={3} star={true} size={"small"}></Rate>
-        <br></br>
-        <Rate count={4} value={3} star={true}></Rate>
-        <br></br>
-        <Rate count={4} value={3} star={true} size={"large"}></Rate>
-        <br></br>
-        <Rate count={4} value={3} star={true} size={"huge"}></Rate>
-        <br></br>
-        <Rate count={4} value={3} star={true} size={"massive"}></Rate>
-      </div>
-
+      <h4 class="ui header">Size</h4>
+      <p>A rating can vary in size</p>
+      <Rate count={4} value={3} star={true} size={"mini"}></Rate>
+      <br></br>
+      <Rate count={4} value={3} star={true} size={"tiny"}></Rate>
+      <br></br>
+      <Rate count={4} value={3} star={true} size={"small"}></Rate>
+      <br></br>
+      <Rate count={4} value={3} star={true}></Rate>
+      <br></br>
+      <Rate count={4} value={3} star={true} size={"large"}></Rate>
+      <br></br>
+      <Rate count={4} value={3} star={true} size={"huge"}></Rate>
+      <br></br>
+      <Rate count={4} value={3} star={true} size={"massive"}></Rate>
     </div>
   }
 }
